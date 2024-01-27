7aa2e4be072f26e768364f2d4cbf5d21d3c47498
UdayTelangeR <157913082+UdayTelangeR@users.noreply.github.com>
2024-01-28 03:09:33 +0530

    Update mainPro.java

--- mainPro.java
+++ mainPro.java
@@ -4,6 +4,6 @@ System.out.println("Hello Uday");
 System.out.println("Hello Asmita");
 System.out.println("Hello Shreyanshi");
 System.out.println("Hello Scooby");
-System.out.println("Hello Mom and Dad");
+System.out.println("Hello Mom and Dad  ");
 }
 }
