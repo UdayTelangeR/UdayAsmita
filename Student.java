c9940acd3771af8db41d23c5fe2e7492bdf864c5
UdayTelangeR <157913082+UdayTelangeR@users.noreply.github.com>
2024-01-28 03:07:23 +0530

    updated

--- Student.java
+++ Student.java
@@ -1,5 +1,6 @@
 public class Student {
     public Student(){
         System.out.println("I am now working professional");
+        System.out.println("I am now working professional worker");
     }
 }
