a4c5d6938d50c52c8af5a69761c31a3c380b1de8
UdayTelangeR <157913082+UdayTelangeR@users.noreply.github.com>
2024-01-28 02:37:25 +0530

    Add files via upload

---
+++ mainPro.java
@@ -0,0 +1,8 @@
+public class mainPro {
+public static void main (String [] args){
+System.out.println("Hello Uday");
+System.out.println("Hello Asmita");
+System.out.println("Hello Shreyanshi");
+System.out.println("Hello Scooby");
+}
+}
